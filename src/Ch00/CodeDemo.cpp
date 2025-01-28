b9619739cbd2ea825e23c5fbdfbf7d3dae780063
acostasjc <111208548+acostasjc@users.noreply.github.com>
2025-01-28 12:08:20 +0000

    change 3

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03 with modifications 2
+// Exercise 00_03 with modifications 3
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
